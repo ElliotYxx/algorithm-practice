85ec5d5a71642ab1cc9df2925139852be46c5462
elliot <wm.lai@trip.com>
2025-06-26 20:24:04 +0800

    - haluo二面

---
+++ src/interview/HaluoTest2.java
@@ -0,0 +1,116 @@
+package interview;
+
+import java.util.List;
+
+/**
+ * @Author: Elliot
+ * @Date: 2025-06-26 19:33
+ * @Description: todo
+ *
+ * a	给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+ * 	b	示例 1：
+ * 	i	输入：head = [1,2,3,4,5], left = 2, right = 4
+ * 	ii	输出：[1,4,3,2,5]
+ * 	c	示例 2：
+ * 	i	输入：head = [5], left = 1, right = 1
+ * 	ii	输出：[5]
+ * 	d	提示：
+ * 	i	链表中节点数目为 n
+ * 	ii	1 <= n <= 500
+ * 	iii	-500 <= Node.val <= 500
+ * 	iv	1 <= left <= right <= n
+ **/
+public class HaluoTest2 {
+
+
+    public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+
+        ListNode curr = l1;
+
+        System.out.println("before reverseList:");
+        while (curr != null){
+            System.out.print(curr.val + "");
+            curr = curr.next;
+        }
+
+        ListNode result = reverseList(l1, 2, 4);
+        curr = result;
+        System.out.println("after reverseList:");
+        while (curr != null){
+            System.out.print(curr.val + "");
+            curr = curr.next;
+        }
+        System.out.println("");
+
+
+        // case 2
+        ListNode a = new ListNode(5);
+        ListNode result2 = reverseList(a, 1, 1);
+
+        curr = result2;
+        while (curr != null){
+            System.out.print(curr.val + "");
+            curr = curr.next;
+        }
+
+
+    }
+
+
+    private static ListNode reverseList(ListNode head, Integer start, Integer end) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        if (start > end){
+            return head;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+
+        for (int i = 1; i < start; i++) {
+            pre = pre.next;
+        }
+
+        ListNode curr = pre.next;
+        ListNode prev = null;
+
+        for (int i = start; i <= end; i++) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        pre.next.next = curr;
+        pre.next = prev;
+        return dummy.next;
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            this.val = x;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ src/practice/day2/Trap.java
@@ -0,0 +1,66 @@
+package practice.day2;
+
+/**
+ * @Author: Elliot
+ * @Date: 2025-06-26 16:11
+ * @Description: todo
+ *
+ * //给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * //
+ * //
+ * //
+ * // 示例 1：
+ * //
+ * //
+ * //
+ * //
+ * //输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * //输出：6
+ * //解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+ * //
+ * //
+ * // 示例 2：
+ * //
+ * //
+ * //输入：height = [4,2,0,3,2,5]
+ * //输出：9
+ **/
+public class Trap {
+
+
+    public static void main(String[] args) {
+        System.out.println(trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
+
+        System.out.println(trap(new int[]{4,2,0,3,2,5}));
+
+    }
+
+    public static int trap(int[] height) {
+
+        if (height == null || height.length == 0){
+            return 0;
+        }
+        int len = height.length;
+        int[] pre = new int[len];
+        pre[0] = height[0];
+        int[] suf = new int[len];
+        suf[len - 1] = height[len - 1];
+
+        for (int i = 1; i < len; i++) {
+            pre[i] = Math.max(pre[i - 1], height[i]);
+        }
+        for (int i = len - 2; i > 0; i--) {
+            suf[i] = Math.max(suf[i + 1], height[i]);
+        }
+
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            int curr = Math.min(suf[i], pre[i]) - height[i];
+            if (curr > 0){
+                res += curr;
+            }
+        }
+
+        return res;
+    }
+}
